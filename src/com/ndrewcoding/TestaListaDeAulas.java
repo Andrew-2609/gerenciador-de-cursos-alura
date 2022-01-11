153f9c25177929ea5674d766bbc3fdbeb827adea
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-11 09:26:52 -0300

    Feat: implement sorting by 'duracao' attribute of Aula

--- src/com/ndrewcoding/TestaListaDeAulas.java
+++ src/com/ndrewcoding/TestaListaDeAulas.java
@@ -2,12 +2,13 @@ package com.ndrewcoding;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class TestaListaDeAulas {
     public static void main(String[] args) {
 
         Aula primeiraAula = new Aula("Revisitando as ArrayLists", 21);
-        Aula segundaAula = new Aula("Listas de Objetos", 15);
+        Aula segundaAula = new Aula("Listas de Objetos", 20);
         Aula terceiraAula = new Aula("Relacionamentos de Listas e Objetos", 15);
 
         ArrayList<Aula> aulas = new ArrayList<>();
@@ -20,7 +21,10 @@ public class TestaListaDeAulas {
 
         Collections.sort(aulas);
         System.out.println("--Após ordenar por *título*--");
+        aulas.forEach(System.out::println);
 
+        aulas.sort(Comparator.comparing(Aula::getDuracao));
+        System.out.println("--Após ordenar por *duração*--");
         aulas.forEach(System.out::println);
 
     }
