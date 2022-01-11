977f8ff9ebf2835da4cfa598f1bd0a63058bf3d1
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-10 22:29:53 -0300

    Feat: add basic implementation of TestaListaDeAulas

--- src/com/ndrewcoding/TestaListaDeAulas.java
+++ src/com/ndrewcoding/TestaListaDeAulas.java
@@ -1,4 +1,21 @@
 package com.ndrewcoding;
 
+import java.util.ArrayList;
+
 public class TestaListaDeAulas {
+    public static void main(String[] args) {
+
+        Aula primeiraAula = new Aula("Revisitando as ArrayLists", 21);
+        Aula segundaAula = new Aula("Listas de Objetos", 15);
+        Aula terceiraAula = new Aula("Relacionamentos de Listas e Objetos", 15);
+
+        ArrayList<Aula> aulas = new ArrayList<>();
+
+        aulas.add(primeiraAula);
+        aulas.add(segundaAula);
+        aulas.add(terceiraAula);
+
+        System.out.println(aulas);
+
+    }
 }
