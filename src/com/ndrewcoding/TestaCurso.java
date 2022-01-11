792d520fbabe94d5c1e8488bde6589383c8ad84f
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-11 20:15:14 -0300

    Feat: add basic implementation of TestaCurso

--- src/com/ndrewcoding/TestaCurso.java
+++ src/com/ndrewcoding/TestaCurso.java
@@ -1,2 +1,15 @@
-package com.ndrewcoding;public class TestaCurso {
+package com.ndrewcoding;
+
+public class TestaCurso {
+    public static void main(String[] args) {
+
+        Curso javaColecoes = new Curso("Dominando as Coleções do Java", "Paulo Silveira");
+
+        javaColecoes.adicionar(new Aula("Trabalhando com ArrayList", 23));
+        javaColecoes.adicionar(new Aula("Criando uma Aula", 15));
+        javaColecoes.adicionar(new Aula("Modelando com Coleções", 21));
+
+        System.out.println(javaColecoes.getAulas());
+
+    }
 }
