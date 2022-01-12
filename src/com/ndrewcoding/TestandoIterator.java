9f5f9c94dea66d2547bdc8ce3195f3b8e749a3a2
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-12 07:06:48 -0300

    Feat: implement Iterator in TestandoIterator

--- src/com/ndrewcoding/TestandoIterator.java
+++ src/com/ndrewcoding/TestandoIterator.java
@@ -1,2 +1,25 @@
-package com.ndrewcoding;public class TestandoIterator {
+package com.ndrewcoding;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@SuppressWarnings("WhileLoopReplaceableByForEach")
+public class TestandoIterator {
+    public static void main(String[] args) {
+
+        List<String> letras = new ArrayList<>();
+        letras.add("A");
+        letras.add("B");
+        letras.add("C");
+        letras.add("D");
+        letras.add("E");
+
+        Iterator<String> iterator = letras.iterator();
+        while (iterator.hasNext()) {
+            String proximaLetra = iterator.next();
+            System.out.println(proximaLetra);
+        }
+
+    }
 }
