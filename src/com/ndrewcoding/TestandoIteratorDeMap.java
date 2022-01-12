737a3e7494610164f75f9bad3b4313b41ee7fc57
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-12 07:35:51 -0300

    Feat: implement different ways to show the content of jogadoresDeBasquete HashMap

--- src/com/ndrewcoding/TestandoIteratorDeMap.java
+++ src/com/ndrewcoding/TestandoIteratorDeMap.java
@@ -1,2 +1,33 @@
-package com.ndrewcoding;public class TestandoIteratorDeMap {
+package com.ndrewcoding;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+@SuppressWarnings("WhileLoopReplaceableByForEach")
+public class TestandoIteratorDeMap {
+    public static void main(String[] args) {
+
+        Map<Integer, String> jogadoresDeBasquete = new HashMap<>();
+        jogadoresDeBasquete.put(1, "Michael Jordan");
+        jogadoresDeBasquete.put(2, "Kobe Bryant");
+        jogadoresDeBasquete.put(3, "LeBron James");
+        jogadoresDeBasquete.put(4, "Tim Duncan");
+
+        Set<Integer> keys = jogadoresDeBasquete.keySet();
+
+        System.out.println("-- Usando forEach --");
+
+        keys.forEach(index -> System.out.println(jogadoresDeBasquete.get(index)));
+
+        System.out.println("\n-- Usando Iterator<Integer> --");
+
+        Iterator<Integer> iterator = keys.iterator();
+        while (iterator.hasNext()) {
+            String jogador = jogadoresDeBasquete.get(iterator.next());
+            System.out.println(jogador);
+        }
+
+    }
 }
