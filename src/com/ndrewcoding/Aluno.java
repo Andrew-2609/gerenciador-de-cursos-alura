1a3cb83dd1f81e44f0b7799c5affab709ce3b704
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-11 21:03:54 -0300

    Feat: implement basic structure of Aluno

--- src/com/ndrewcoding/Aluno.java
+++ src/com/ndrewcoding/Aluno.java
@@ -1,2 +1,21 @@
-package com.ndrewcoding;public class Aluno {
+package com.ndrewcoding;
+
+public class Aluno {
+
+    private String nome;
+    private int numeroDeMatricula;
+
+    public Aluno(String nome, int numeroDeMatricula) {
+        this.nome = nome;
+        this.numeroDeMatricula = numeroDeMatricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getNumeroDeMatricula() {
+        return numeroDeMatricula;
+    }
+
 }
